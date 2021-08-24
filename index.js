fad6d0990e71629090a50d7b8935b3c3cfd90638
Adam <inmon.adam@yahoo.com>
2021-08-24 18:46:08 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+main.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+document.body.append(newHeader);
+
+const h1 = document.getElementById("victory");
+
+newHeader.innerHTML = "Adam is the champion";
+
+
